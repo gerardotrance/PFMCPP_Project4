6408a3edf2f26698fb189cd0d594f93b4add5358
gerardotrance <gezcoulter247@gmail.com>
2020-04-13 23:06:01 +0000

    getting started on project 4 part1

--- main.cpp
+++ main.cpp
@@ -40,6 +40,53 @@ send me a DM to check your pull request
 
  Wait for my code review.
  */
+struct FloatType
+{
+    
+    float add(float lhs, float rhs);
+    float subtract(float lhs, float rhs);
+    float multiply(float lhs, float rhs);
+    float divide(float lhs, float rhs);
+
+};
+struct DoubleType
+{
+    
+    double add(double lhs, double rhs);
+    double subtract(double lhs, double rhs);
+    double multiply(double lhs, double rhs);
+    double divide(double lhs, double rhs);
+
+};
+struct IntType
+{
+    
+    int add(int lhs, int rhs);
+    int subtract(int lhs, int rhs);
+    int multiply(int lhs, int rhs);
+    int divide(int lhs, int rhs);
+
+};
+
+float FloatType::add(float lhs, float rhs)
+{
+    return lhs + rhs;
+}
+
+float FloatType::subtract(float lhs, float rhs)
+{
+    return lhs + rhs;
+}
+
+float FloatType::multiply(float lhs, float rhs)
+{
+    return lhs * rhs;
+}
+
+float FloatType::divide(float lhs, float rhs)
+{
+    return lhs / rhs;
+}
 
 #include <iostream>
 int main()
